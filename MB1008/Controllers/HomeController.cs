277e54599ca3aaf880d916b163c1b65829e6c99c
Volkan <volkanculhaci@hotmail.com>
2023-08-12 00:08:20 +0300

    - The conversion works on the home page! - CSS adjustments.

--- MB1008/Controllers/HomeController.cs
+++ MB1008/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MB1008.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Diagnostics;
@@ -42,37 +43,37 @@ namespace MB1008.Controllers
             return View("Index", _dbContext.ExchangeRates.ToList()); // Return the updated view with exchange rates
         }
 
-        [HttpGet]
-        public async Task<IActionResult> UpdateExchangeRates()
-        {
-            var httpClient = _httpClientFactory.CreateClient();
-            var html = await httpClient.GetStringAsync("https://www.tcmb.gov.tr/kurlar/today.xml");
+        //[HttpGet]
+        //public async Task<IActionResult> UpdateExchangeRates()
+        //{
+        //    var httpClient = _httpClientFactory.CreateClient();
+        //    var html = await httpClient.GetStringAsync("https://www.tcmb.gov.tr/kurlar/today.xml");
 
-            var doc = new XmlDocument();
-            doc.LoadXml(html);
+        //    var doc = new XmlDocument();
+        //    doc.LoadXml(html);
 
-            var currencyNodes = doc.SelectNodes("//tr");
-            foreach (XmlNode currencyNode in currencyNodes)
-            {
-                var fromCurrency = currencyNode.SelectSingleNode("td[@class='para kurkodu']").InnerText.Trim();
-                var toCurrency = currencyNode.SelectSingleNode("td[@class='para birim']").InnerText.Trim();
-                var buyRate = decimal.Parse(currencyNode.SelectSingleNode("td[@class='deger']").InnerText.Trim());
+        //    var currencyNodes = doc.SelectNodes("//tr");
+        //    foreach (XmlNode currencyNode in currencyNodes)
+        //    {
+        //        var fromCurrency = currencyNode.SelectSingleNode("td[@class='para kurkodu']").InnerText.Trim();
+        //        var toCurrency = currencyNode.SelectSingleNode("td[@class='para birim']").InnerText.Trim();
+        //        var buyRate = decimal.Parse(currencyNode.SelectSingleNode("td[@class='deger']").InnerText.Trim());
 
-                var exchangeRate = new ExchangeRate
-                {
-                    FromCurrency = fromCurrency,
-                    ToCurrency = toCurrency,
-                    BuyRate = buyRate,
-                    Date = DateTime.UtcNow
-                };
+        //        var exchangeRate = new ExchangeRate
+        //        {
+        //            FromCurrency = fromCurrency,
+        //            ToCurrency = toCurrency,
+        //            BuyRate = buyRate,
+        //            Date = DateTime.UtcNow
+        //        };
 
-                _dbContext.ExchangeRates.Add(exchangeRate);
-            }
+        //        _dbContext.ExchangeRates.Add(exchangeRate);
+        //    }
 
-            await _dbContext.SaveChangesAsync();
+        //    await _dbContext.SaveChangesAsync();
 
-            return RedirectToAction("Index"); // Redirect to your desired page
-        }
+        //    return RedirectToAction("Index"); // Redirect to your desired page
+        //}
 
         public IActionResult TCMB_DATA()
         {
@@ -85,7 +86,7 @@ namespace MB1008.Controllers
         }
 
         // last added
-        [HttpPost]
+        [HttpGet]
         public IActionResult GetExchangeRate(string fromCurrency, string toCurrency, DateTime selectedDate)
         {
             var exchangeRate = _dbContext.ExchangeRates
