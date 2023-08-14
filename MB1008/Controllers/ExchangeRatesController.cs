456197341605c759624dc869595b8629c7d751a2
Volkan <volkanculhaci@hotmail.com>
2023-08-14 22:32:49 +0300

    There is a problem when TRY is selected. It will be fixed. Function will be added to the Exchange Rates Table.

--- MB1008/Controllers/ExchangeRatesController.cs
+++ MB1008/Controllers/ExchangeRatesController.cs
@@ -24,7 +24,7 @@ namespace MB1008.Controllers
             string formattedDate = selectedDate.ToString("dd-MM-yyyy");
             Debug.WriteLine($"Formatted Date: {formattedDate}");
 
-            string url = $"https://evds2.tcmb.gov.tr/service/evds/series=TP_DK_RUB_A-TP.DK.USD.S.YTL-TP.DK.EUR.S.YTL-TP.DK.CHF.S.YTL-TP.DK.GBP.S.YTL-TP.DK.JPY.S.YTL&startDate={formattedDate}&endDate={formattedDate}&type=xml&key=jettt0PrGN";
+            string url = $"https://evds2.tcmb.gov.tr/service/evds/series=TP_DK_RUB_A-TP.DK.USD.A.YTL-TP.DK.EUR.A.YTL-TP.DK.CHF.A.YTL-TP.DK.GBP.A.YTL-TP.DK.JPY.A.YTL-TP.DK.AUD.A-TP.DK.DKK.A-TP.DK.KWD.A-TP.DK.NOK.A&startDate={formattedDate}&endDate={formattedDate}&type=xml&key=jettt0PrGN";
             //string url = $"https://evds2.tcmb.gov.tr/service/evds/series=TP.DK.USD.S.YTL-TP.DK.EUR.S.YTL-TP.DK.CHF.S.YTL-TP.DK.GBP.S.YTL-TP.DK.JPY.S.YTL&startDate=10-08-2023&endDate=10-08-2023&type=xml&key=jettt0PrGN";
 
             HttpResponseMessage response = await _httpClient.GetAsync(url);
@@ -37,12 +37,17 @@ namespace MB1008.Controllers
                 var exchangeRates = doc.Descendants("items").Select(item => new
                 {
                     date = item.Element("Tarih").Value,
-                    eur = string.IsNullOrEmpty(item.Element("TP_DK_EUR_S_YTL").Value) ? "0" : item.Element("TP_DK_EUR_S_YTL").Value,
-                    usd = string.IsNullOrEmpty(item.Element("TP_DK_USD_S_YTL").Value) ? "0" : item.Element("TP_DK_USD_S_YTL").Value,
-                    chf = string.IsNullOrEmpty(item.Element("TP_DK_CHF_S_YTL").Value) ? "0" : item.Element("TP_DK_CHF_S_YTL").Value,
-                    gbp = string.IsNullOrEmpty(item.Element("TP_DK_GBP_S_YTL").Value) ? "0" : item.Element("TP_DK_GBP_S_YTL").Value,
-                    jpy = string.IsNullOrEmpty(item.Element("TP_DK_JPY_S_YTL").Value) ? "0" : item.Element("TP_DK_JPY_S_YTL").Value,
+                    eur = string.IsNullOrEmpty(item.Element("TP_DK_EUR_A_YTL").Value) ? "0" : item.Element("TP_DK_EUR_A_YTL").Value,
+                    usd = string.IsNullOrEmpty(item.Element("TP_DK_USD_A_YTL").Value) ? "0" : item.Element("TP_DK_USD_A_YTL").Value,
+                    chf = string.IsNullOrEmpty(item.Element("TP_DK_CHF_A_YTL").Value) ? "0" : item.Element("TP_DK_CHF_A_YTL").Value,
+                    gbp = string.IsNullOrEmpty(item.Element("TP_DK_GBP_A_YTL").Value) ? "0" : item.Element("TP_DK_GBP_A_YTL").Value,
+                    jpy = string.IsNullOrEmpty(item.Element
+("TP_DK_JPY_A_YTL").Value) ? "0" : item.Element("TP_DK_JPY_A_YTL").Value,
                     rub = string.IsNullOrEmpty(item.Element("TP_DK_RUB_A").Value) ? "0" : item.Element("TP_DK_RUB_A").Value,
+                    aud = string.IsNullOrEmpty(item.Element("TP_DK_AUD_A").Value) ? "0" : item.Element("TP_DK_AUD_A").Value,
+                    dkk = string.IsNullOrEmpty(item.Element("TP_DK_DKK_A").Value) ? "0" : item.Element("TP_DK_DKK_A").Value,
+                    kwd = string.IsNullOrEmpty(item.Element("TP_DK_KWD_A").Value) ? "0" : item.Element("TP_DK_KWD_A").Value,
+                    nok = string.IsNullOrEmpty(item.Element("TP_DK_NOK_A").Value) ? "0" : item.Element("TP_DK_NOK_A").Value,
                 });
 
                 XElement root = new XElement("ExchangeRates",
@@ -53,8 +58,12 @@ namespace MB1008.Controllers
                         new XElement("eur", ex.eur),
                         new XElement("che", ex.chf),
                         new XElement("gbp", ex.gbp),
-                        new XElement("jpy", ex.jpy)
-                    )
+                        new XElement("jpy", ex.jpy),
+                        new XElement("rub", ex.rub),
+                        new XElement("aud", ex.aud),
+                        new XElement("dkk", ex.dkk),
+                        new XElement("kwd", ex.kwd),
+                        new XElement("nok", ex.nok))
                 );
 
                 return Content(root.ToString(), "application/xml");
--- MB1008/wwwroot/js/currency-converter.js
+++ MB1008/wwwroot/js/currency-converter.js
@@ -1,78 +1,64 @@
 async function getExchangeRateFromDatabase(selectedDate) {
     const apiUrl = `/Home/GetExchangeRateFromDatabase?selectedDate=${selectedDate.toISOString()}`;
 
-    try {
-        const response = await fetch(apiUrl);
-        if (response.ok) {
-            const data = await response.json();
-            if (data.success) {
-                console.log("VERITABANINDAN VERI CEKILDI");
-                return data.exchangeRates;
-            }
+    const response = await fetch(apiUrl);
+    if (response.ok) {
+        const data = await response.json();
+        if (data.success) {
+            console.log("VERITABANINDAN VERI CEKILDI");
+            return data.exchangeRates;
         }
-    } catch (error) {
-        console.error("Error fetching data from the database:", error);
     }
-
+    console.error("Error fetching data from the database");
     return null;
 }
 
 async function saveExchangeRatesToDatabase(exchangeRates, selectedDate) {
     const apiUrl = `/Home/SaveExchangeRatesToDatabase?selectedDate=${selectedDate.toISOString()}`;
 
-    try {
-        const response = await fetch(apiUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(exchangeRates),
-        });
+    const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(exchangeRates),
+    });
 
-        if (response.ok) {
-            const data = await response.json();
-            return data.success;
-        } else {
-            console.error("Error saving exchange rates:", response.statusText);
-        }
-    } catch (error) {
-        console.error("Error saving exchange rates:", error);
+    if (response.ok) {
+        const data = await response.json();
+        return data.success;
+    } else {
+        console.error("Error saving exchange rates:", response.statusText);
+        return false;
     }
-
-    return false;
 }
 
 async function getExchangeRateFromTCMB(fromCurrency, toCurrency, selectedDate) {
     const apiUrl = `api/ExchangeRates?selectedDate=${selectedDate.toISOString()}`;
-    //const apiUrl = `/ExchangeRates/GetExchangeRate?fromCurrency=${fromCurrency}&toCurrency=${toCurrency}&selectedDate=${selectedDate.toISOString()}`;
     console.log(apiUrl);
 
-    try {
-        const response = await fetch(apiUrl);
-        if (response.ok) {
-            console.log("response okey");
+    const response = await fetch(apiUrl);
+    if (response.ok) {
+        console.log("response okey");
 
-            const xmlString = await response.text();
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+        const xmlString = await response.text();
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlString, "text/xml");
 
-            console.log("xml Dokumanina bakiyoruz", xmlDoc);
-            const exchangeRates = {};
+        console.log("xml Dokumanina bakiyoruz", xmlDoc);
+        const exchangeRates = {};
 
-            const rateElements = xmlDoc.querySelectorAll("items > *");
-            rateElements.forEach((rateElement) => {
-                const currency = rateElement.tagName;
-                const rate = parseFloat(rateElement.textContent);
-                exchangeRates[currency] = rate;
-                exchangeRates["date"] = selectedDate.toISOString();
-            });
-            console.log("exchangeRates", exchangeRates);
-            return exchangeRates;
-        }
-    } catch (error) {
-        console.error("Error fetching exchange rate from TCMB:", error);
+        const rateElements = xmlDoc.querySelectorAll("items > *");
+        rateElements.forEach((rateElement) => {
+            const currency = rateElement.tagName;
+            const rate = parseFloat(rateElement.textContent);
+            exchangeRates[currency] = rate;
+            exchangeRates["date"] = selectedDate.toISOString();
+        });
+        console.log("exchangeRates", exchangeRates);
+        return exchangeRates;
     }
-
+    console.error("Error fetching exchange rate from TCMB");
     return null;
 }
 
@@ -95,11 +81,11 @@ async function getExchangeRate(fromCurrency, toCurrency, selectedDate) {
 }
 
 function calculateConvertedAmount(amount, rate) {
-    console.log("Calculating converted amount...");
+    console.log("Calculating converted amount... +", rate);
     return (amount * rate).toFixed(4);
 }
 
-function displayConvertedAmount(convertedAmount) {
+function displayConvertedAmount(convertedAmount, fromCurrency) {
     console.log("Displaying converted amount:", convertedAmount);
     document.getElementById("converted-amount").value = convertedAmount;
 }
@@ -121,74 +107,98 @@ async function updateConvertedAmount() {
     console.log("Target currency:", toCurrency);
     console.log("Selected date:", selectedDate);
 
-    try {
-        const existingExchangeRates = await getExchangeRateFromDatabase(
-            selectedDate
+    const existingExchangeRates = await getExchangeRateFromDatabase(selectedDate);
+
+    if (existingExchangeRates) {
+        const selectedRateFrom = existingExchangeRates.find(
+            (rate) => rate.fromCurrency === fromCurrency
         );
-        console.log("VERITABANINDAKI VAR OLAN KURLARA BAKILIYOR");
-        console.log(existingExchangeRates);
+        const selectedRateTo = existingExchangeRates.find(
+            (rate) => rate.fromCurrency === toCurrency
+        );
 
-        if (existingExchangeRates) {
-            console.log("DATABASE'DE MEVCUT");
-            const selectedRate = existingExchangeRates.find(
-                (rate) => rate.fromCurrency === fromCurrency
-            );
-            if (selectedRate) {
-                console.log("bu database'de bu para birimi mevcut");
-
+        if (fromCurrency === "TRY") {
+            console.log("TRY SECILDI");
+            if (toCurrency === "TRY") {
+                const convertedAmount = calculateConvertedAmount(amount, 1);
+                displayConvertedAmount(convertedAmount, fromCurrency);
+            } else if (selectedRateTo) {
+                console.log("selectedRateTo bulundu", selectedRateTo);
                 const convertedAmount = calculateConvertedAmount(
                     amount,
-                    selectedRate.buyRate
+                    1 / selectedRateTo.buyRate
                 );
-                displayConvertedAmount(convertedAmount);
-                return;
+                displayConvertedAmount(convertedAmount, fromCurrency);
+            } else {
+                console.log("Hedef para birimi veritabanýnda bulunamadý.");
+                displayErrorMessage("Hedef para birimi veritabanýnda bulunamadý.");
             }
+        } else if (toCurrency === "TRY") {
+            if (selectedRateFrom) {
+                const convertedAmount = calculateConvertedAmount(
+                    amount * selectedRateFrom.buyRate,
+                    1
+                );
+                displayConvertedAmount(convertedAmount, fromCurrency);
+            } else {
+                console.log("Kaynak para birimi veritabanýnda bulunamadý.");
+                displayErrorMessage("Kaynak para birimi veritabanýnda bulunamadý.");
+            }
+        } else if (selectedRateFrom && selectedRateTo) {
+            const convertedAmount = calculateConvertedAmount(
+                amount * selectedRateFrom.buyRate,
+                1 / selectedRateTo.buyRate
+            );
+            displayConvertedAmount(convertedAmount, fromCurrency);
+        } else {
+            console.log("Kurlar veritabanýnda bulunamadý.");
+            displayErrorMessage("Kurlar veritabanýnda bulunamadý.");
         }
-            
-        console.log("getExchangeRateFromDatabase calismadi. Veriler TCMB'den alinacak");
-        console.log(selectedDate);
+    } else {
+        console.log("Veritabanýndan kurlar bulunamadý, TCMB'den alýnýyor...");
         const exchangeRates = await getExchangeRateFromTCMB(fromCurrency, toCurrency, selectedDate);
 
         if (exchangeRates) {
-            console.log("TCMB KURLARI ALINDI:", exchangeRates);
-            const convertedAmount = calculateConvertedAmount(
-                amount,
-                exchangeRates[fromCurrency.toLowerCase()] // Assuming currency codes are in lowercase
-            );
-            displayConvertedAmount(convertedAmount);
+            const fromRate = exchangeRates[fromCurrency.toLowerCase()];
+            const toRate = exchangeRates[toCurrency.toLowerCase()];
 
-            // Create an array of exchange rate objects
-            const exchangeRateObjects = [];
-            for (const currency in exchangeRates) {
-                if (currency !== "date") {
-                    exchangeRateObjects.push({
-                        fromCurrency: currency.toUpperCase(), // Assuming you want to save currencies in uppercase
-                        toCurrency: toCurrency,
-                        buyRate: exchangeRates[currency],
-                        date: exchangeRates.date,
-                    });
+            if (fromRate && toRate) {
+                const convertedAmount = calculateConvertedAmount(
+                    amount * fromRate,
+                    1 / toRate
+                );
+
+                displayConvertedAmount(convertedAmount, fromCurrency);
+
+                const exchangeRateObjects = [];
+                for (const currency in exchangeRates) {
+                    if (currency !== "date") {
+                        exchangeRateObjects.push({
+                            fromCurrency: currency.toUpperCase(),
+                            toCurrency: "TRY",
+                            buyRate: exchangeRates[currency],
+                            date: exchangeRates.date,
+                        });
+                    }
                 }
-            }
 
-            // Save the fetched exchange rates to the database
-            const savedToDatabase = await saveExchangeRatesToDatabase(
-                exchangeRateObjects,
-                selectedDate
-            );
+                const savedToDatabase = await saveExchangeRatesToDatabase(
+                    exchangeRateObjects,
+                    selectedDate
+                );
 
-            if (savedToDatabase) {
-                console.log("Exchange rates saved to the database.");
+                if (savedToDatabase) {
+                    console.log("Kurlar veritabanýna kaydedildi.");
+                } else {
+                    console.error("Kurlar veritabanýna kaydedilemedi.");
+                }
             } else {
-                console.error("Failed to save exchange rates to the database.");
+                console.log("Kurlar veritabanýndan alýnýrken hata oluþtu.");
+                console.log("Veri mevcut deðil.");
+                displayErrorMessage("Veri mevcut deðil.");
             }
         } else {
-            console.log("KURLAR ALINIRKEN HATA OLUSTU");
-            console.log("No exchange rate data available.");
-            displayErrorMessage("No data available for the selected criteria.");
+            console.log("Kurlar TCMB'den alýnýrken hata oluþtu.");
         }
-    } catch (error) {
-        console.error("Error:", error);
-        displayErrorMessage("An error occurred.");
     }
-
 }
